6f79e3bd33a12abd488e31a5c347828e54e3f3c6
ritaeperjesi <eperjesirit@gmail.com>
2020-03-20 20:17:05 +0100

    felho1blue

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
   cloudH = random(30,75);
   randomNumber=random(0,100);
   if (mouseIsPressed){
-  image(felho1, mouseX+1, mouseY+1, cloudW, cloudH);
+  image(felho1blue, mouseX+1, mouseY+1, cloudW, cloudH);
 }
     if (keyIsPressed) { 
       c=255;
